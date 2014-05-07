e0f7cce41d515f9de1eaaa98a270d367b63efda9
Lampros <weirdbricks@myopera.com>
2014-05-06 22:34:40 -0700

    some minor updates

--- reload-jail.sh
+++ reload-jail.sh
@@ -2,9 +2,12 @@
 echo "checking if jail is running.."
 jail1_status=`ezjail-admin list | grep jail1 | grep -v grep |wc -l`
 
+ipaddress=192.168.2.101
+nic1=nfe0
+
 create_jail(){
 	echo "creating jail1"
-  	ezjail-admin create jail1 192.168.2.20 > /dev/null 2>&1
+  	ezjail-admin create jail1 $ipaddress > /dev/null 2>&1
         echo "copying resolv.conf file"
         cp /etc/resolv.conf /usr/jails/jail1/etc/resolv.conf
         echo "remove /usr/bin symlink"
@@ -17,14 +20,14 @@ create_jail(){
 	mkdir /usr/jails/jail1/usr/ports
 	echo "mounting host's ports"
 	mount_nullfs -o ro /usr/ports/ /usr/jails/jail1/usr/ports
-	echo "creating ifconfig alias for 192.168.2.20"
-	ifconfig re0 alias 192.168.2.20 netmask 255.255.255.0
+	echo "creating ifconfig alias for $ipaddress"
+	ifconfig $nic1 alias $ipaddress netmask 255.255.255.0
 	echo "modifying /usr/local/etc/ezjail/jail1 to allow pinging"
 	awk '{ gsub("export jail_jail1_parameters=\"\"","export jail_jail1_parameters=\"allow.raw_sockets=1 allow.sysvipc=1\"" ) ; print }' /usr/local/etc/ezjail/jail1 > /usr/local/etc/ezjail/jail1-edited
 	rm /usr/local/etc/ezjail/jail1
 	mv /usr/local/etc/ezjail/jail1-edited /usr/local/etc/ezjail/jail1
         echo "modifying SSHd configuration"
-	awk '{ gsub("#ListenAddress 0.0.0.0","ListenAddress 192.168.2.20" ) ; print }' /usr/jails/jail1/etc/ssh/sshd_config > /usr/jails/jail1/etc/ssh/sshd_config_edited
+	awk '{ gsub("#ListenAddress 0.0.0.0","ListenAddress '$ipaddress'" ) ; print }' /usr/jails/jail1/etc/ssh/sshd_config > /usr/jails/jail1/etc/ssh/sshd_config_edited
         rm /usr/jails/jail1/etc/ssh/sshd_config
 	awk '{ gsub("#PermitRootLogin no","PermitRootLogin yes"); print }' /usr/jails/jail1/etc/ssh/sshd_config_edited > /usr/jails/jail1/etc/ssh/sshd_config
 #	mv /usr/jails/jail1/etc/ssh/sshd_config_edited /usr/jails/jail1/etc/ssh/sshd_config
@@ -33,7 +36,8 @@ create_jail(){
         echo 'creating directory for ssh'
 	mkdir /usr/jails/jail1/root/.ssh
         echo "copying ansible-master's key"
-	cat /usr/jails/ansible-master/root/.ssh/id_rsa.pub >> /usr/jails/jail1/root/.ssh/authorized_keys
+	cat ~/.ssh/id_rsa.pub >> /usr/jails/jail1/root/.ssh/authorized_keys
+	cat ~/.ssh/windows.pub >> /usr/jails/jail1/root/.ssh/authorized_keys
         echo "setting correct SSH key permissions"
         chmod 600 /usr/jails/jail1/root/.ssh/*
 	chmod 700 /usr/jails/jail1/root
@@ -50,6 +54,8 @@ delete_jail(){
 	ezjail-admin delete jail1
 	echo "fixing permissions"
 	chflags -R noschg /usr/jails/jail1
+	echo "deleting old key from known_hosts"
+        sed -i.'' '/'$ipaddress'/d' ~/.ssh/known_hosts
 	echo "deleting remaining files"
 	rm -r -f /usr/jails/jail1
 	echo "jail completely deleted"
