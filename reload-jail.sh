2f9bcc6400ee567c40b98b7ebba77bfc859eac4d
Lampros <weirdbricks@myopera.com>
2014-05-13 17:38:49 -0700

    command line arguments added to create more than one jails

--- reload-jail.sh
+++ reload-jail.sh
@@ -1,8 +1,24 @@
 #!/usr/bin/env sh
-echo "checking if jail is running.."
-jail1_status=`ezjail-admin list | grep jail1 | grep -v grep |wc -l`
+jail=$1
+ipaddress=$2
 
-ipaddress=192.168.2.101
+if [ -z "$jail" ]
+then
+  echo "jail name not provided - stopping"
+  exit
+fi
+
+if [ -z "$ipaddress" ]
+then
+  echo "ipaddress not provided - stopping"
+  exit
+fi  
+
+echo "checking if jail: $jail is running.."
+jail_status=`ezjail-admin list | grep $jail | grep -v grep | wc -l`
+echo "the jail status is: $jail_status"
+
+ipaddress=$2
 nic1=`netstat -rn | grep default | awk '{print $6}'`
 
 echo "NIC: $nic1"
@@ -11,8 +27,10 @@ echo "IP: $ipaddress"
 notify() {
   if [ $? -eq 0 ];
   then
+            echo $?
             echo "OK"
   else
+            echo $?
             echo "STOP: Something went wrong!"
             exit
   fi
@@ -20,68 +38,75 @@ notify() {
 
 
 create_jail(){
-	echo "creating jail1"
-  	ezjail-admin create jail1 $ipaddress > /dev/null 2>&1 && notify
+	echo "creating jail: $jail"
+  	ezjail-admin create $jail $ipaddress > /dev/null 2>&1 || notify
         echo "copying resolv.conf file"
-        cp /etc/resolv.conf /usr/jails/jail1/etc/resolv.conf && notify
+        cp /etc/resolv.conf /usr/jails/$jail/etc/resolv.conf || notify
         echo "remove /usr/bin symlink"
-        rm /usr/jails/jail1/usr/bin && notify
+        rm /usr/jails/$jail/usr/bin || notify
         echo "copy the /usr/bin/ directory"
-        cp -R -p /usr/jails/basejail/usr/bin /usr/jails/jail1/usr/ > /dev/null && notify
+        cp -R -p /usr/jails/basejail/usr/bin /usr/jails/$jail/usr/ > /dev/null || notify
 	echo "remove /usr/ports from jail"
-	rm /usr/jails/jail1/usr/ports && notify
+	rm /usr/jails/$jail/usr/ports || notify
 	echo "create directory to mount host's ports in"
-	mkdir /usr/jails/jail1/usr/ports && notify
+	mkdir /usr/jails/$jail/usr/ports || notify
 	echo "mounting host's ports"
-	mount_nullfs -o ro /usr/ports/ /usr/jails/jail1/usr/ports && notify
+	mount_nullfs -o ro /usr/ports/ /usr/jails/$jail/usr/ports || notify
 	echo "creating ifconfig alias for $ipaddress"
-	ifconfig $nic1 alias $ipaddress netmask 255.255.255.0 && notify
-	echo "modifying /usr/local/etc/ezjail/jail1 to allow pinging" 
-	awk '{ gsub("export jail_jail1_parameters=\"\"","export jail_jail1_parameters=\"allow.raw_sockets=1 allow.sysvipc=1\"" ) ; print }' /usr/local/etc/ezjail/jail1 > /usr/local/etc/ezjail/jail1-edited && notify
-	rm /usr/local/etc/ezjail/jail1 && notify
-	mv /usr/local/etc/ezjail/jail1-edited /usr/local/etc/ezjail/jail1 && notify
+	ifconfig $nic1 alias $ipaddress netmask 255.255.255.0 || notify
+	echo "modifying /usr/local/etc/ezjail/$jail to allow pinging" 
+	awk '{ gsub("export jail_'$jail'_parameters=\"\"","export jail_'$jail'_parameters=\"allow.raw_sockets=1 allow.sysvipc=1\"" ) ; print }' /usr/local/etc/ezjail/$jail > /usr/local/etc/ezjail/$jail-edited || notify
+	rm /usr/local/etc/ezjail/$jail || notify
+	mv /usr/local/etc/ezjail/$jail-edited /usr/local/etc/ezjail/$jail || notify
         echo "modifying SSHd configuration"
-	awk '{ gsub("#ListenAddress 0.0.0.0","ListenAddress '$ipaddress'" ) ; print }' /usr/jails/jail1/etc/ssh/sshd_config > /usr/jails/jail1/etc/ssh/sshd_config_edited && notify
-        rm /usr/jails/jail1/etc/ssh/sshd_config && notify
-	awk '{ gsub("#PermitRootLogin no","PermitRootLogin yes"); print }' /usr/jails/jail1/etc/ssh/sshd_config_edited > /usr/jails/jail1/etc/ssh/sshd_config && notify
+	awk '{ gsub("#ListenAddress 0.0.0.0","ListenAddress '$ipaddress'" ) ; print }' /usr/jails/$jail/etc/ssh/sshd_config > /usr/jails/$jail/etc/ssh/sshd_config_edited || notify
+        rm /usr/jails/$jail/etc/ssh/sshd_config || notify
+	awk '{ gsub("#PermitRootLogin no","PermitRootLogin yes"); print }' /usr/jails/$jail/etc/ssh/sshd_config_edited > /usr/jails/$jail/etc/ssh/sshd_config || notify
         echo "setting SSH to start" 
-	echo 'sshd_enable="YES"' > /usr/jails/jail1/etc/rc.conf && notify
+	echo 'sshd_enable="YES"' > /usr/jails/$jail/etc/rc.conf || notify
         echo 'creating directory for ssh'
-	mkdir /usr/jails/jail1/root/.ssh && notify
+	mkdir /usr/jails/$jail/root/.ssh || notify
         echo "copying ansible-master's key"
-	cat ~/.ssh/id_rsa.pub >> /usr/jails/jail1/root/.ssh/authorized_keys && notify
-	cat ~/.ssh/windows.pub >> /usr/jails/jail1/root/.ssh/authorized_keys && notify
+	cat ~/.ssh/id_rsa.pub >> /usr/jails/$jail/root/.ssh/authorized_keys || notify
+	cat ~/.ssh/windows.pub >> /usr/jails/$jail/root/.ssh/authorized_keys || notify
         echo "setting correct SSH key permissions" 
-        chmod 600 /usr/jails/jail1/root/.ssh/* && notify
-	chmod 700 /usr/jails/jail1/root && notify
-	echo "OK: jail ready to use - starting it up"
-        ezjail-admin start jail1 && notify
-	echo "Jail started - bootstrapping pkg manager"
+        chmod 600 /usr/jails/$jail/root/.ssh/* || notify
+	chmod 700 /usr/jails/$jail/root || notify
+	echo "OK: jail: $jail ready to use - starting it up"
+        ezjail-admin start $jail || notify
+	echo "Jail: $jail started - bootstrapping pkg manager"
 }
 
 delete_jail(){
-	echo "stopping the jail"
-	ezjail-admin stop jail1 && notify
-	echo "unmount ports"
-	umount /usr/jails/jail1/usr/ports && notify
+	echo "stopping jail: $jail"
+	ezjail-admin stop $jail || notify
+        checkmount=`mount | grep $jail -c`
+        echo "going to check for a mounted port tree: $checkmount"
+	if [ $checkmount -eq 0 ];
+        then
+          echo "ports were not mounted for $jail"
+        else
+	  echo "unmount ports"
+          umount /usr/jails/$jail/usr/ports || notify
+        fi
 	echo "deleting the jail" 
-	ezjail-admin delete jail1 && notify
+	ezjail-admin delete $jail || notify
 	echo "fixing permissions"
-	chflags -R noschg /usr/jails/jail1 && notify
+	chflags -R noschg /usr/jails/$jail || notify
 	echo "deleting old key from known_hosts"
-        sed -i.'' '/'$ipaddress'/d' ~/.ssh/known_hosts && notify
+        sed -i.'' '/'$ipaddress'/d' ~/.ssh/known_hosts || notify
 	echo "deleting remaining files"
-	rm -r -f /usr/jails/jail1 && notify
+	rm -r -f /usr/jails/$jail || notify
 	echo "jail completely deleted"
 
 }
 
-if [ $jail1_status -eq 0 ];
+if [ $jail_status -eq 0 ];
 then 
-	echo "jail doesn't exist - getting it ready"
-        create_jail
+	echo "jail: $jail doesn't exist - getting it ready"
+       create_jail
 else
-	echo "yup, jail is running - deleting"
+	echo "yup, jail: $jail is running - deleting"
 	delete_jail
 	echo "and rebuilding"
 	create_jail
